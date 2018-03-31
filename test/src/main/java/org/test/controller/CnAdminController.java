ff5c9c185500d9d143905baeb66fb727148cacfe
278831970@qq.com <278831970@qq.com>
2018-03-31 09:46:29 +0700

    run ok

--- test/src/main/java/org/test/controller/CnAdminController.java
+++ test/src/main/java/org/test/controller/CnAdminController.java
@@ -9,22 +9,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.test.service.ICnAdminService;
+
+import com.alibaba.fastjson.JSON;
+
 import org.test.entity.CnAdmin;
 import org.test.impl.CnAdminServiceImpl;
 
+import org.apache.log4j.Logger;
+
+// http://localhost:8080/test/admin/getAdminInfo?id=1
 @Controller
 @RequestMapping("/admin")
 public class CnAdminController {
+	private static Logger logger = Logger.getLogger(CnAdminController.class);
 	
 	@Autowired
 	private CnAdminServiceImpl  inAdminServiceImpl;
 	
-	@RequestMapping("method=getAdminInfo")
+	@RequestMapping("/getAdminInfo")
 	public String getAdminInfo(HttpServletRequest request,Model model) {
 		int adminId = Integer.parseInt(request.getParameter("id"));
-		CnAdmin  cnAdmin  = this.inAdminServiceImpl.getAdminInfoByid(adminId);
+		logger.info("adminId:"+ JSON.toJSONString(adminId) );
 		
+		CnAdmin  cnAdmin  = this.inAdminServiceImpl.getAdminInfoByid(adminId);
+		logger.info("cnAdmin:"+ JSON.toJSONString(cnAdmin) );
+		
+		model.addAttribute("test", "hello test demo");
 		model.addAttribute("adminInfo", cnAdmin);
-		return "login";
+		return "admin/login";
 	}
 }
